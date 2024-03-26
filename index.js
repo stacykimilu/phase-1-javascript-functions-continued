5f50939035bb0431097f68ba55ecdf77328254e0
Stacy <stacykimilu@gmail.com>
2024-03-26 09:14:34 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("*");
+  console.log(encouragingPromptFunction("a hard worker")); // Output: "You are *a hard worker*!"
+  
+  const encouragingPromptFunction2 = wrapAdjective("||");
+  console.log(encouragingPromptFunction2("a dedicated programmer")); // Output: "You are ||a dedicated programmer||!"
+  
